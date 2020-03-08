2c9f7d2a63aabbb5c7373c49b9e4514e502ca6b0
H S <sekhah@my.lanecc.edu>
2020-03-08 15:43:29 -0700

    Added controllers and views.

---
+++ TermProject/Controllers/RestaurantReviewController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TermProject.Controllers
+{
+    public class RestaurantReviewController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Search() => View();
+
+        public IActionResult Add() => View();
+    }
+}
---
+++ TermProject/Controllers/SuccessSecretController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TermProject.Controllers
+{
+    public class SuccessSecretController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Search() => View();
+
+        public IActionResult Add() => View();
+    }
+}
