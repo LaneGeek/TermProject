b585b136cd8d59392d54cdef0d59f94051996b30
H S <sekhah@my.lanecc.edu>
2020-03-08 19:53:52 -0700

    Added views, viewmodels and controllers for Identity.

---
+++ TermProject/Controllers/AccountController.cs
@@ -0,0 +1,94 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TermProject.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace TermProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMgr, SignInManager<AppUser> signinMgr)
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel details, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result =
+                        await signInManager.PasswordSignInAsync(user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginViewModel.Email),
+                    "Invalid email or password and I ain't telling you which!");
+            }
+
+            return View(details);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        public ViewResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(CreateUserViewModel createUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    FirstName = createUserViewModel.FirstName,
+                    LastName = createUserViewModel.LastName,
+                    UserName = createUserViewModel.Email,
+                    Email = createUserViewModel.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(user, createUserViewModel.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View("Register", createUserViewModel);
+        }
+    }
+}
