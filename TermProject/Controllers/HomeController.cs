e1c240351a1f1d059e7e2febde1d70fb468dce02
H S <sekhah@my.lanecc.edu>
2020-03-11 10:18:47 -0700

    Added user's name on homepage.

--- TermProject/Controllers/HomeController.cs
+++ TermProject/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TermProject.Models;
 
 namespace TermProject.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly UserManager<AppUser> _userManager;
+        
+        public HomeController(UserManager<AppUser> userManager) => _userManager = userManager;
+
+        public async Task<IActionResult> Index() => View(await _userManager.GetUserAsync(User));
 
         public IActionResult About() => View();
     }
